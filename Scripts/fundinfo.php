e84c8bee974f6eeadc5f690177f4d226496070a3
José Ballester <jose@ballester.me>
2016-12-12 23:14:10 +0100

    Added Morningstar fundinfo

---
+++ Scripts/fundinfo.php
@@ -0,0 +1,38 @@
+<?php
+// Mutual fund info collection
+// Source: Morningstar.es
+// José Ballester, 12 Dec 2016
+
+// Uses Simple HTML DOM Parser
+include('inc/simple_html_dom.php');
+
+/* 	Input: fund's ISIN as a GET variable
+		Available options:
+		- 'value' (default): last Net Asset Value (NAV)
+		- 'date' : date of the last NAV */
+
+if(!isset($_GET['isin'])) {
+	echo "ISIN not provided.";
+} else {
+	$isin = $_GET['isin'];
+
+	// Step 1: Getting MStar's fund code
+	$html = str_get_html(file_get_contents("http://www.morningstar.es/es/funds/SecuritySearchResults.aspx?search=".$isin));
+	$fundlink = $html->find('table[id=ctl00_MainContent_fundTable] td a', 0);
+	if(!isset($fundlink)) {
+		echo "ISIN not valid.";
+	} else {
+		$html = str_get_html(file_get_contents("https://www.morningstar.es".$fundlink->getAttribute("href")));
+		$option = (isset($_GET['option']) ? $_GET['option'] : "");
+		if($option == "date") {
+			// Option 'date'
+			$fundVLdate = $html->find('div[id=overviewQuickstatsDiv] table td[class="line heading"] span[class=heading]', 0)->innertext;
+	  	echo substr($fundVLdate, 6);
+		} else {
+			// Option 'value' or default
+			$fundVL = $html->find('div[id=overviewQuickstatsDiv] table td[class="line text"]', 0)->innertext;
+			echo str_replace(",", ".", substr($fundVL, 5));
+		}
+	}
+}
+?>
